e231c8ba9893334105708262fc2f88373fef987b
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2025-02-11 18:34:22 +0200

    Create a get_quotes function that queries the website, parses the HTML, and creates a list of Quote objects

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,8 @@
 from dataclasses import dataclass
+from bs4 import BeautifulSoup, Tag
+import requests
+
+BASE_URL = 'https://quotes.toscrape.com/'
 
 
 @dataclass
@@ -8,8 +12,23 @@ class Quote:
     tags: list[str]
 
 
+def parse_quotes(quote: Tag) -> Quote:
+    print(Quote(
+        text=quote.select_one(".text").text,
+        author=quote.select_one(".author").text,
+        tags=[tag.text for tag in quote.select(".tag")]
+    ))
+
+
+def get_quotes() -> [Quote]:
+    text = requests.get(BASE_URL).content
+    soup = BeautifulSoup(text, 'html.parser')
+    quotes = soup.select(".quote")
+    return [parse_quotes(quote) for quote in quotes]
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    print(get_quotes())
 
 
 if __name__ == "__main__":
